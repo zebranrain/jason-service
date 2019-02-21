8a299faa8794564fdf62c938b0c734f9b3495cf5
Jason Bourget <jason.andre.bourget@gmail.com>
2019-02-20 19:24:59 -0500

    Modified folder structure

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const bodyParser = require('body-parser');
-const retrievePriceHistory = require('./db/queries.js');
+const retrievePriceHistory = require('../db/queries.js');
 
 const translateError = {
   companyNotFound: { code: 404, message: "Sorry! We don't have price records for that company." },
