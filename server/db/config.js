49b7a02655ef88b152e10525514b06e7f7c62dbd
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-02 16:39:43 -0700

    Added RDS

--- server/db/config.js
+++ server/db/config.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize('stock-prices', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
   pool: {
     max: 5,
@@ -11,7 +12,9 @@ const sequelize = new Sequelize('stock-prices', process.env.DB_USERNAME, process
     acquire: 30000,
     idle: 10000
   },
-  logging: false
+  // logging: false
 });
+
+console.log('port', process.env.DB_PORT);
 
 module.exports = sequelize;
