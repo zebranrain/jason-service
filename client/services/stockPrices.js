bd30798ba36b3691ba5d34815a360ed79964793a
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-02 15:14:05 -0700

    Testing different routes

--- client/services/stockPrices.js
+++ client/services/stockPrices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async (ticker, timeframe) => {
-  const response = await axios.get('http://jason-service.fzdbudezre.us-west-2.elasticbeanstalk.com:3001/api/prices', {
+  const response = await axios.get('http://18.237.9.220:3001/api/prices', {
     params: { ticker, timeframe }
   });
   return response.data;
