90cae15309a4d4611a6bbd0a159998d337b70a5b
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-02 15:19:05 -0700

    Modified routes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 var cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const retrievePriceHistory = require('./db/queries.js');
 
