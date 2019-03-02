d8ddfd68da5829f21d85baeef932db35b4f5add2
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-02 15:27:02 -0700

    Changed port

--- client/services/stockPrices.js
+++ client/services/stockPrices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async (ticker, timeframe) => {
-  const response = await axios.get('http://jason-service.fzdbudezre.us-west-2.elasticbeanstalk.com:3001/api/prices', {
+  const response = await axios.get('http://jason-service.fzdbudezre.us-west-2.elasticbeanstalk.com/api/prices', {
     params: { ticker, timeframe }
   });
   return response.data;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 var cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 80;
 const bodyParser = require('body-parser');
 const retrievePriceHistory = require('./db/queries.js');
 
