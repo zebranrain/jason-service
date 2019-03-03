f39622f34bcc3960eb2e5237da5af9eb42de7e96
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-02 18:11:57 -0700

    trying relative paths for requests & scripts

--- client/services/stockPrices.js
+++ client/services/stockPrices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async (ticker, timeframe) => {
-  const response = await axios.get('http://jason-service.fzdbudezre.us-west-2.elasticbeanstalk.com/api/prices', {
+  const response = await axios.get('api/prices', {
     params: { ticker, timeframe }
   });
   return response.data;
