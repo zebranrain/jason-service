31bbfa38542923c82ec4d018ae153312e3b679c1
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-06 12:19:13 -0700

    Swapped Moment for date-fns to improve bundle.js delivery speed

--- client/utilities/convertDateToString.js
+++ client/utilities/convertDateToString.js
@@ -1,14 +1,16 @@
-import moment from 'moment';
+import { format } from 'date-fns';
 
 /* Formats milliseconds to be human readable and formatted
  according to the selected timeframe */
 
 export default function convertDateToString(ms, timeframe) {
+  const time = new Date(ms);
   if (timeframe === 'day') {
-    return `${moment(ms).format('h:mm A')} ET`;
+    console.log();
+    return `${format(time, 'h:mm A')} ET`;
   }
   if (timeframe === 'week') {
-    return `${moment(ms).format('h:mm A, MMM D')} ET`;
+    return `${format(time, 'h:mm A, MMM D')} ET`;
   }
-  return moment(ms).format('MMM D, YYYY');
+  return format(time, 'MMM D, YYYY');
 }
