0ce40b8b3fa708ed345bb48a5ce5b9aa40ff8cf6
Jason Bourget <jason.andre.bourget@gmail.com>
2019-03-06 11:43:57 -0700

    Completed moving marker

--- client/components/Chart.jsx
+++ client/components/Chart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { XYPlot, LineSeries, Crosshair } from 'react-vis';
+import { XYPlot, LineSeries, Crosshair, MarkSeries } from 'react-vis';
 import '../styles/Chart.scss';
 import Ticker from './Ticker.jsx';
 import Change from './Change.jsx';
@@ -9,15 +9,20 @@ class Chart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      crosshairValues: [{}] // initialized with expected data shape, an array with one object
+      crosshairValues: [{}], // initialized with expected data shape, an array with one object
+      markerData: [{ x: 1, y: 1, size: 1 }],
+      crosshairDisplay: 'block' // toggles between 'block' and 'none' to turn off crosshair when mouse leaves chart
     };
     this.slider = this.slider.bind(this);
   }
 
   /* Crosshair movement event handler */
   slider(value, event) {
-    this.setState({ crosshairValues: [value] });
-    console.log(event);
+    this.setState({
+      crosshairDisplay: 'block',
+      crosshairValues: [value],
+      markerData: [{ x: value.x, y: value.y, size: 1 }]
+    });
   }
 
   /* Determines the price that should be passed down to the Price component */
@@ -32,7 +37,7 @@ class Chart extends React.Component {
   }
 
   render() {
-    const { pricepoints, company, timeframe } = this.props;
+    const { pricepoints, timeframe } = this.props;
     const openingPrice = pricepoints[0] ? pricepoints[0].y : 0;
     const price = this.setPrice(pricepoints);
     const date = convertDateToString(
@@ -40,22 +45,42 @@ class Chart extends React.Component {
       timeframe
     );
 
+    const crossLineStyle = {
+      line: {
+        height: '160px',
+        display: this.state.crosshairDisplay
+      }
+    };
+
+    const toggleVisibility = {
+      display: this.state.crosshairDisplay
+    };
+
     return (
       <div>
         <Ticker price={price} />
         <Change openingPrice={openingPrice} currentPrice={price} />
         <XYPlot
-          onMouseLeave={() => this.setState({ crosshairValues: [{}] })}
           width={676}
           height={196}
+          onMouseLeave={() => this.setState({ crosshairValues: [{}], markerData: [{}], crosshairDisplay: 'none' })}
         >
           <LineSeries
             data={pricepoints}
             onNearestX={this.slider}
             color="#21ce99"
           />
-          <Crosshair values={this.state.crosshairValues} style={{ line: { height: '160px' } }}>
-            <div>
+          <MarkSeries
+            className="mark-series"
+            sizeRange={[1, 5]}
+            data={this.state.markerData}
+            stroke="white"
+            fill="#21ce99"
+            strokeWidth={2}
+            style={toggleVisibility}
+          />
+          <Crosshair values={this.state.crosshairValues} style={crossLineStyle}>
+            <div style={toggleVisibility}>
               <p>{date}</p>
             </div>
           </Crosshair>
